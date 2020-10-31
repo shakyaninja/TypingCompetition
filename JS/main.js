af2f5ea0e3ac2361e08ac68fb596f26797dd461b
Luja Shakya <7052957-shakya445@users.noreply.gitlab.com>
2020-10-31 08:38:35 +0545

    adds login,register page with index page but not banner yet

---
+++ JS/main.js
@@ -0,0 +1,73 @@
+// var sel1 = document.getElementById("sel1");
+// var sel2 = document.getElementById("sel2");
+// var sel3 = document.getElementById("sel3");
+// var sel4 = document.getElementById("sel4");
+// var sel5 = document.getElementById("sel5");
+// var sel6 = document.getElementById("sel6");
+// var sel7 = document.getElementById("sel7");
+// var sel8 = document.getElementById("sel8");
+// var radialMenu = document.getElementById("radialMenu");
+
+// sel1.addEventListener('mouseover',load1)
+// sel2.addEventListener('mouseover',load2)
+// sel3.addEventListener('mouseover',load3)
+// sel4.addEventListener('mouseover',load4)
+// sel5.addEventListener('mouseover',load5)
+// sel6.addEventListener('mouseover',load6)
+// sel7.addEventListener('mouseover',load7)
+// sel8.addEventListener('mouseover',load8)
+// sel1.addEventListener('mouseout',mouseOut)
+// sel2.addEventListener('mouseout',mouseOut)
+// sel3.addEventListener('mouseout',mouseOut)
+// sel4.addEventListener('mouseout',mouseOut)
+// sel5.addEventListener('mouseout',mouseOut)
+// sel6.addEventListener('mouseout',mouseOut)
+// sel7.addEventListener('mouseout',mouseOut)
+// sel8.addEventListener('mouseout',mouseOut)
+
+// function load1(){
+//     radialMenu.style.background = " url('../Assets/Components/sel_1.png') no-repeat";
+//     radialMenu.style.backgroundSize = "contain";
+//     radialMenu.style.transition = "0.5s";
+// }
+// function load2(){
+//     radialMenu.style.background = " url('../Assets/Components/sel_2.png') no-repeat";
+//     radialMenu.style.backgroundSize = "contain";
+//     radialMenu.style.transition = "0.5s";
+// }
+// function load3(){
+//     radialMenu.style.background = " url('../Assets/Components/sel_3.png') no-repeat";
+//     radialMenu.style.backgroundSize = "contain";
+//     radialMenu.style.transition = "0.5s";
+// }
+// function load4(){
+//     radialMenu.style.background = " url('../Assets/Components/sel_4.png') no-repeat";
+//     radialMenu.style.backgroundSize = "contain";
+//     radialMenu.style.transition = "0.5s";
+// }
+// function load5(){
+//     radialMenu.style.background = " url('../Assets/Components/sel_5.png') no-repeat";
+//     radialMenu.style.backgroundSize = "contain";
+//     radialMenu.style.transition = "0.5s";
+// }
+// function load6(){
+//     radialMenu.style.background = " url('../Assets/Components/sel_6.png') no-repeat";
+//     radialMenu.style.backgroundSize = "contain";
+//     radialMenu.style.transition = "0.5s";
+// }
+// function load7(){
+//     radialMenu.style.background = " url('../Assets/Components/sel_7.png') no-repeat";
+//     radialMenu.style.backgroundSize = "contain";
+//     radialMenu.style.transition = "0.5s";
+// }
+// function load8(){
+//     radialMenu.style.background = " url('../Assets/Components/sel_8.png') no-repeat";
+//     radialMenu.style.backgroundSize = "contain";
+//     radialMenu.style.transition = "0.5s";
+// }
+
+// function mouseOut() {
+//     radialMenu.style.background = "url('../Assets/Components/unselected.png') no-repeat";
+//     radialMenu.style.backgroundSize = "contain";
+//     radialMenu.style.transition = "0.5s";
+//   }
